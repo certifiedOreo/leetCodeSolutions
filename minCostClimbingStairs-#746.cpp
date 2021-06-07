5373c298738fa734448e2ccee5b6b5a14a7b4507
Ajeet Yadav <ajeet19010@iiitd.ac.in>
2021-06-07 15:45:22 +0530

    leetcode-#746 | June Challenge | day-7

---
+++ minCostClimbingStairs-#746.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        
+        int dp[n];
+        dp[0] = cost[0];
+        dp[1]= cost[1];
+        
+        for(int i=2;i<n; i++){
+            dp[i] = cost[i]+min(dp[i-1], dp[i-2]);
+        }
+        
+                for(int i=0;i<n; i++){
+            cout<<dp[i]<<" ";
+        }
+        
+        
+        
+        
+        return min(dp[n-1], dp[n-2]);
+        
+        
+    }
+};
