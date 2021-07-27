49f7a0a50c85ece6df6041fc7866ec754a5a0b9c
Ajeet Yadav <ajeet19010@iiitd.ac.in>
2021-07-27 14:07:22 +0530

    leetcode-#44 | top-down dp approach

---
+++ WildcardMatching.java
@@ -0,0 +1,54 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        
+        int m = p.length();
+        int n = s.length();
+        
+        boolean [][] dp = new  boolean[m+1][n+1];
+        
+        dp[0][0]= true;
+        
+        for(int i=1; i<=n; i++){
+            dp[0][i]=false;
+        }
+        
+        for(int i=1; i<=m; i++){
+            if(p.charAt(i-1)=='?'){
+                dp[i][0]=false;
+            }
+            else if(p.charAt(i-1)=='*'){
+                dp[i][0] = dp[i-1][0];
+            }
+            else{
+                dp[i][0] = false;
+            }
+        }
+        
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n; j++){
+                if(p.charAt(i-1)==s.charAt(j-1) || p.charAt(i-1)=='?'){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else if(p.charAt(i-1)=='*'){
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                
+                    
+            }
+                else{
+                    dp[i][j]  = false;
+                }
+        }
+        
+    }
+        
+        
+        
+        return dp[m][n];
+        
+        
+        
+        
+        
+        
+}
+}
